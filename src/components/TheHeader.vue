6a0979e13618418c2d69d7492e8f09c62acd2fb3
LIs1459 <Lis1459@yandex.ru>
2026-02-27 17:18:37 +0300

    added the logout button to the header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,34 @@
 <script>
+import store from "@/store"
 import { RouterLink, RouterView } from "vue-router"
+export default {
+  computed: {
+    isAuthenticated() {
+      return store.getters.isAuthenticated
+    },
+  },
+  methods: {
+    handleLogout(evt) {
+      try {
+        store.dispatch("logout")
+      } catch (error) {
+        console.log(error.message)
+      }
+    },
+  },
+}
 </script>
 
 <template>
   <header class="header">
-    <div class="header__logo"><img src="../assets/Group 1000006015.png" alt="logo" />TipMinder</div>
+    <div class="header__logo"><img src="../assets/logo.svg" alt="logo" />TipMinder</div>
     <nav class="navigation">
       <RouterLink to="/contracts" class="navigation__item">Contracts</RouterLink>
       <RouterLink to="/details" class="navigation__item">Details</RouterLink>
     </nav>
-    <RouterLink to="/login" class="header__log-in">Log in</RouterLink>
+    <RouterLink to="/login" class="header__button" v-if="!isAuthenticated">Log in</RouterLink>
+    <button class="header__button header__log-out" v-else @click="handleLogout">Log out</button>
+    <!-- <button class="header__button header__log-out" v-else>Log out</button> -->
   </header>
 </template>
 
@@ -40,10 +59,38 @@ import { RouterLink, RouterView } from "vue-router"
   padding: 27px 16px;
 }
 
-.header__log-in {
+.header__button {
+  position: relative;
+  cursor: pointer;
+  line-height: 18px;
+  border: none;
   border-radius: 10px;
   background: var(--background-accent-gradient);
-  padding: 15px;
+  padding: 15px 16px;
   color: var(--color-buttons);
+}
+
+.header__log-out {
+  padding-left: 64px;
+}
+
+.header__log-out::before {
+  content: url(../assets/profile.svg);
+  position: absolute;
+  left: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+
+.header__log-out::after {
+  content: "";
+  position: absolute;
+  border: 1px solid #ffdac5;
+  right: 88px;
+  height: 48px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,9 +7,11 @@ import "@fontsource/inter/600.css"
 import { createApp } from "vue"
 import App from "./App.vue"
 import router from "./router"
+// import store from "./store"
 
 const app = createApp(App)
 
 app.use(router)
+// app.use(store)
 
 app.mount("#app")
